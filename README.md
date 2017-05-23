c3a6c2e614812e122d034189c6b0541ba6770c12
everaldomatias <matiaseveraldo@gmail.com>
2017-05-22 21:29:25 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Automatic Custom Login Logo #
+**Contributors:** [everaldomatias](https://profiles.wordpress.org/everaldomatias)  
+**Tags:** custom logo, login page,  
+**Requires at least:** 4.0  
+**Tested up to:** 4.5  
+**Stable tag:** 0.1  
+**License:** GPLv2 or later  
+**License URI:** http://www.gnu.org/licenses/gpl-2.0.html  
+
+WordPress plugin that changes out the default logo on the login screen with the custom logo.
+
+## Description ##
+
+This plugin uses the image set in the Custom Login Panel of the administration to the logo displayed on the login page of the site.
+
+### Contribute ###
+
+You can contribute to the source code in our [GitHub](https://github.com/everaldomatias/automatic-custom-logo-login) page.
+
+### Need help or want to make a suggestion? ###
+
+Create an issue in our [GitHub](https://github.com/everaldomatias/automatic-custom-logo-login/issues) page.
+
+## Changelog ##
+
+### 0.1 ###
+
+- Initial version.
---
+++ automatic-custom-logo-login.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Plugin Name: Automatic Custom Login Logo
+ * Plugin URI: https://github.com/everaldomatias/automatic-custom-logo-login
+ * Description: WordPress plugin that changes out the default logo on the login screen with the custom logo.
+ * Version: 0.1
+ * Author: Everaldo Matias
+ * Author URI: http://everaldomatias.github.io
+ * License: GPLv2 or later
+ */
+
+// If this file is called directly, abort
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'Automatic_Custom_Login_Logo' ) ) :
+
+	/**
+	 * Automatic Custom Login Logo plugin.
+	 */
+	class Automatic_Custom_Login_Logo {
+
+		/**
+		 * Initialize the plugin.
+		 */
+		function __construct() {
+
+			// Verify version WP.
+			global $wp_version;
+
+			if ( version_compare( $wp_version, '4.5', '>=' ) ) {
+				
+				// Add Login Logo URL 
+				add_filter( 'login_headerurl', array( $this, 'acll_login_logo_url' ) );
+
+				// Add Login Logo Title
+				add_filter( 'login_headertitle', array( $this, 'acll_login_logo_title' ) );
+
+				// Add Custom Logo Image in Login Page
+				add_action( 'login_enqueue_scripts', array( $this, 'acll_login_logo' ) );
+
+			} else {
+				add_action( 'admin_notices', array( $this, 'acll_is_missing_notice' ) );
+			}
+
+		}
+
+		/**
+		 * Instance of this class.
+		 *
+		 * @var object.
+		 */
+		protected static $instance = null;
+
+		/**
+		 * Return an instance of this class.
+		 *
+		 * @return object a single instance of this class.
+		 */
+		public static function get_instance() {
+			// If the single instance hasn't been set, set it now.
+			if ( null == self::$instance ) {
+				self::$instance = new self;
+			}
+			return self::$instance;
+		}
+
+		/**
+		 * Automatic Custom Login Logo missing notice.
+		 *
+		 * @return string admin notice.
+		 */
+		public function acll_is_missing_notice() {
+			echo '<div class="error"><p><strong>' . __( 'Automatic Custom Login Logo', 'acll' ) . '</strong> ' . sprintf( __( 'works only with the version 4.5 or later of the WordPress, please %s!', 'acll' ), '<a href="' . esc_url( admin_url( 'update-core.php' ) ) . '">' . __( 'upgrade your installation', 'acll' ) . '</a>' ) . '</p></div>';
+		}
+
+		/**
+		 * Return home url in custom logo.
+		 *
+		 * @return home url.
+		 */
+		public function acll_login_logo_url() {
+			return esc_url( get_home_url() );
+		}
+
+		/**
+		 * Return name site in custom logo.
+		 *
+		 * @return string name site.
+		 */
+		public function acll_login_logo_title() {
+			return get_bloginfo( 'name' );
+		}
+
+		/**
+		 * Return the custom logo in login page.
+		 *
+		 * @return css with the custom login.
+		 */
+
+		public function acll_login_logo() {
+			if ( has_custom_logo() ) {
+				$logo = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' ); ?>
+			    <style type="text/css">
+			    	/* Automatic Custom Login Logo Styles */
+			        body.login div#login h1 a {
+			            background: url( '<?php echo esc_url( $logo[0] ); ?>' ) center no-repeat !important;
+			            background-size: 80% !important;
+			            height: <?php echo esc_html( $logo[1] ); ?>px !important;
+			            width: <?php echo esc_html( $logo[2] ); ?>px !important;
+			        }
+			    </style>
+			<?php
+			}
+		}
+	}
+
+	/**
+	 * Initialize the plugin actions.
+	 */
+	add_action( 'plugins_loaded', array( 'Automatic_Custom_Login_Logo', 'get_instance' ) );
+
+endif;
